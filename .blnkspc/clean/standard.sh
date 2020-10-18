ee6f72b06ea4140a011a65c618e5c11748c3406e
ensoxyz <ceo@enso.xyz>
2020-10-18 18:55:29 +0000

    -/=

--- .blnkspc/clean/standard.sh
+++ .blnkspc/clean/standard.sh
@@ -1,28 +1,23 @@
-#!
-
-sudo apt purge 
-	#amd64-microcode \ 	#standard
-	bash-completion \ 	#standard
-	#bc \ 			#standard
-	#bind9-dnsutils \ 	#standard
-	#bind9-host \ 		#standard
-	#bind9-libs \ 		#standard
-	#cron \ 		#standard
-	#dc \ 			#standard
-	#ftp \ 			#standard
-	#gettext-base \ 	#standard
-	#hunspell-en-us \ 	#standard
-	#intel-microcode \ 	#standard
-	#libsasl2-2 \ 		#standard
-	#libsasl2-modules-db \ 	#standard
-	#lsof \ 		#standard
-	#manpages \ 		#standard
-	#openssh-client \ 	#standard
-	#pciutils \ 		#standard
-	#perl \ 		#standard
-	#perl-modules-5.30 \ 	#standard
-	#telnet \ 		#standard
-	#time \ 		#standard
-	#wamerican 		#standard
-sudo apt autoremove
-
+sap 	#amd64-microcode \ 	#standard
+sap 	#bash-completion \ 	#standard
+sap 	#bc \ 			#standard
+sap 	#bind9-dnsutils \ 	#standard
+sap 	#bind9-host \ 		#standard
+sap 	#bind9-libs \ 		#standard
+sap 	#cron \ 		#standard
+sap 	#dc \ 			#standard
+sap 	#ftp \ 			#standard
+sap 	#gettext-base \ 	#standard
+sap 	#hunspell-en-us \ 	#standard
+sap 	#intel-microcode \ 	#standard
+sap 	#libsasl2-2 \ 		#standard
+sap 	#libsasl2-modules-db \ 	#standard
+sap 	#lsof \ 		#standard
+sap 	#manpages \ 		#standard
+sap 	#openssh-client \ 	#standard
+sap 	#pciutils \ 		#standard
+sap 	#perl \ 		#standard
+sap 	#perl-modules-5.30 \ 	#standard
+sap 	#telnet \ 		#standard
+sap 	#time \ 		#standard
+sap 	#wamerican 		#standard
